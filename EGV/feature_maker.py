0a84b3ebaf29241a2ea6540f095d9d5b3927e4ab
Sjoerd Boeschoten <sboeschoten@hhdelfland.nl>
2022-07-14 14:14:00 +0200

    added standard run for ts functions

--- EGV/feature_maker.py
+++ EGV/feature_maker.py
@@ -4,17 +4,20 @@ import timeseries_functions as tf
 
 
 def main():
-    print(1)
+    datasets = tf.tsdf_standard_run(
+        locatie='parkhaven',
+        threshold=24,
+        interpolate=12
+    )
+
+
 
 
 if __name__ == '__main__':
     main()
-    egv_db = tp.egv_standard_run('parkhaven')
-    tsdf = tf.tsdf_interpolate_small_gaps(egv_db)
-    tsdf['hour'] = [tsdf.index[i].hour for i in range(len(tsdf))]
-    tsdf['weekday'] = [tsdf.index[i].weekday for i in range(len(tsdf))]
-    tsdf['monthday'] = [tsdf.index[i].days_in_month for i in range(len(tsdf))]
-    tsdf['month'] = [tsdf.index[i].month for i in range(len(tsdf))]
-
-
-    print(tsdf.tail(10))
+    # egv_db = tp.egv_standard_run('parkhaven')
+    # tsdf = tf.tsdf_interpolate_small_gaps(egv_db)
+    # tsdf['hour'] = [tsdf.index[i].hour for i in range(len(tsdf))]
+    # tsdf['weekday'] = [tsdf.index[i].weekday for i in range(len(tsdf))]
+    # tsdf['monthday'] = [tsdf.index[i].days_in_month for i in range(len(tsdf))]
+    # tsdf['month'] = [tsdf.index[i].month for i in range(len(tsdf))]
