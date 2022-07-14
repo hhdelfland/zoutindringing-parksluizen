37c5778f1c3a80892752b0e83106106f322dd7f4
Sjoerd Boeschoten <sboeschoten@hhdelfland.nl>
2022-07-14 16:50:22 +0200

    init modelling / modelling notebook

--- EGV/feature_maker.py
+++ EGV/feature_maker.py
@@ -11,13 +11,11 @@ def main():
     )
 
 
-
-
 if __name__ == '__main__':
     main()
     # egv_db = tp.egv_standard_run('parkhaven')
     # tsdf = tf.tsdf_interpolate_small_gaps(egv_db)
     # tsdf['hour'] = [tsdf.index[i].hour for i in range(len(tsdf))]
     # tsdf['weekday'] = [tsdf.index[i].weekday for i in range(len(tsdf))]
-    # tsdf['monthday'] = [tsdf.index[i].days_in_month for i in range(len(tsdf))]
+    # tsdf['monthday'] =[tsdf.index[i].days_in_month for i in range(len(tsdf))]
     # tsdf['month'] = [tsdf.index[i].month for i in range(len(tsdf))]
